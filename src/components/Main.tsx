292cd92803ba444d2e204d3e33d77a3224b41900
qbin <huinalam@gmail.com>
2021-01-26 16:15:38 +0900

    다운로드 URL 변경

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -14,7 +14,7 @@ export default class Main extends React.Component<MainProps, any> {
           <img src={BannerImage} alt=""/>
         </div>
         <div className="link-button">
-          <a href="https://voiceearp.blob.core.windows.net/download/comportwomenoftheempire.kr/%ED%99%88%ED%94%BC_%EB%8F%99%EC%95%84%EC%8B%9C%EC%95%84%20%ED%99%94%ED%95%B4%EC%99%80%20%ED%8F%89%ED%99%94%EC%9D%98%20%EB%AA%A9%EC%86%8C%EB%A6%AC%20%EA%B8%B0%EB%B6%80%EA%B8%88%20%EB%AA%A8%EC%A7%91%EC%99%84%EB%A3%8C%20%EB%B3%B4%EA%B3%A0.hwp">
+          <a href="https://voiceearp.blob.core.windows.net/download/comportwomenoftheempire.kr/%ED%99%88%ED%94%BC_%EA%B8%B0%EB%B6%80%EA%B8%88%20%EB%B3%B4%EA%B3%A02021.pdf">
             <button>
               <div>
                 기부금 모집 보고
@@ -23,7 +23,7 @@ export default class Main extends React.Component<MainProps, any> {
           </a>
         </div>
         <div className="link-button2">
-          <a href="https://voiceearp.blob.core.windows.net/download/comportwomenoftheempire.kr/%ED%99%88%ED%94%BC_%EB%8F%99%EC%95%84%EC%8B%9C%EC%95%84%20%ED%99%94%ED%95%B4%EC%99%80%20%ED%8F%89%ED%99%94%EC%9D%98%20%EB%AA%A9%EC%86%8C%EB%A6%AC%20%EA%B8%B0%EB%B6%80%EA%B8%88%20%EB%AA%A8%EC%A7%91%EC%99%84%EB%A3%8C%20%EB%B3%B4%EA%B3%A0.hwp">
+          <a href="https://voiceearp.blob.core.windows.net/download/comportwomenoftheempire.kr/%ED%99%88%ED%94%BC_%EA%B8%B0%EB%B6%80%EA%B8%88%20%EB%B3%B4%EA%B3%A02021.pdf">
             <button>
               <div>
                 기부금 모집 보고
