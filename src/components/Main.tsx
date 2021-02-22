1ac591a8552ad6e8b8d0c90b1d41400be9bd2b21
qbin <huinalam@gmail.com>
2021-02-22 18:31:49 +0900

    다시 기부금 받도록 변경

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -11,14 +11,11 @@ export default class Main extends React.Component<MainProps, any> {
     return (
       <div className="main">
         <div className="banner">
-          <img src={BannerImage} alt="" />
+          <img src={BannerImage} alt=""/>
         </div>
         <div className="link-button">
-          <a href="https://voiceearp.blob.core.windows.net/download/comportwomenoftheempire.kr/%ED%99%88%ED%94%BC_%EA%B8%B0%EB%B6%80%EA%B8%88%20%EB%B3%B4%EA%B3%A02021.pdf">
+          <a href="https://voiceofearp.kr/donation">
             <button>
-              <div>
-                기부금 모집 보고
-              </div>
             </button>
           </a>
         </div>
