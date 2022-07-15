fbe2536a64e55beef1a579a3a932356f1bdf5243
qbin <huinalam@gmail.com>
2022-07-15 11:31:20 +0900

    2022, 2021 기부금 현황 보이기

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -20,7 +20,7 @@ export default class Main extends React.Component<MainProps, any> {
           </a>
         </div>
         <div className="link-button2">
-          <a href="https://voiceearp.blob.core.windows.net/download/comportwomenoftheempire.kr/%ED%99%88%ED%94%BC_%EA%B8%B0%EB%B6%80%EA%B8%88%20%EB%B3%B4%EA%B3%A02021.pdf">
+          <a href="https://voiceearp.blob.core.windows.net/download/comportwomenoftheempire.kr/%EA%B8%B0%EB%B6%80%EA%B8%88%20%EB%AA%A8%EC%A7%91%EC%99%84%EB%A3%8C%20%EB%B3%B4%EA%B3%A0.zip">
             <button>
               <div>
                 기부금 모집 보고
