6c8a8b0bbb05fc18e4c82d8262740a8bb1ae54ea
qbin <huinalam@gmail.com>
2022-08-30 22:47:27 +0900

    후원버튼 삭제

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
 import { Route, BrowserRouter as Router, Link } from 'react-router-dom';
 import './Main.styl';
-const BannerImage = require('../../assets/sitemainbanner.jpg');
+
+const BannerImage = require('../../assets/sitemainbanner-completed.png');
 
 export interface MainProps {
 }
@@ -13,12 +14,12 @@ export default class Main extends React.Component<MainProps, any> {
         <div className="banner">
           <img src={BannerImage} alt=""/>
         </div>
-        <div className="link-button">
+        {/* <div className="link-button">
           <a href="https://voiceofearp.kr/donation">
             <button>
             </button>
           </a>
-        </div>
+        </div> */}
         <div className="link-button2">
           <a href="https://voiceearp.blob.core.windows.net/download/comportwomenoftheempire.kr/%EA%B8%B0%EB%B6%80%EA%B8%88%20%EB%AA%A8%EC%A7%91%EC%99%84%EB%A3%8C%20%EB%B3%B4%EA%B3%A0.zip">
             <button>
